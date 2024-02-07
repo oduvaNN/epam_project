8a4e27b7e317e8363d869654de07693554e5f543
Ilona <oduvan@gmail.com>
2024-02-07 23:18:06 +0200

    created train pipeline

---
+++ src/train/train.py
@@ -0,0 +1,101 @@
+from torch import nn
+import torch
+from src.train.model import SentimentRNN
+from torch.utils.data import DataLoader
+import numpy as np
+import os
+from pathlib import Path
+from datetime import datetime
+
+EXPERIMENT_NAME = f"sentimentalRNN_{datetime.now()}"
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = torch.device(device)
+
+parent = Path(__file__).parent
+model_path = os.path.join(parent, "../../outputs", EXPERIMENT_NAME)
+Path(model_path).mkdir(parents=True, exist_ok=True)
+
+# TODO: create dataloader (maybe in separate file)
+
+
+class TrainPipeline:
+    def __init__(self, num_layers, vocab, embedding_dim, hidden_dim, lr, batch_size):
+        self.model = SentimentRNN(num_layers, len(vocab) + 1, hidden_dim, embedding_dim)
+
+        self.model.to(device)
+        self.batch_size = batch_size
+        self.criterion = nn.BCELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+        self.train_loss = []
+        self.eval_loss = []
+        self.train_acc = []
+        self.eval_acc = []
+
+        self.best_loss = torch.inf
+
+    @staticmethod
+    def accuracy(pred, label):
+        pred = torch.round(pred.squeeze())
+        return torch.sum(pred == label.squeeze()).item()
+
+    def train(self, epochs, train_loader: DataLoader, test_loader: DataLoader):
+
+        for epoch in epochs:
+            print(f"Epoch {epoch}")
+            tr_ls = []  # train losses for batches in this epoch
+            ev_ls = []  # same but eval loss
+            tr_acc = 0.0
+            self.model.train()
+            h = self.model.init_hidden(self.batch_size)
+
+            for inputs, labels in train_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                h = tuple([each.data for each in h])
+
+                self.model.zero_grad()
+                out, h = self.model(inputs, h)
+
+                loss = self.criterion(out.squeeze(), labels.float())
+
+                loss.backward()
+                tr_ls.append(loss.item())
+
+                accuracy = TrainPipeline.accuracy(out, labels)
+                tr_acc += accuracy / len(train_loader.dataset)
+
+                nn.utils.clip_grad_norm(self.model.parameters(), 5)
+                self.optimizer.step()
+
+            eval_h = self.model.init_hidden(self.batch_size)
+            ev_acc = 0.0
+            self.model.eval()
+
+            for inputs, labels in test_loader:
+                eval_h = tuple([each.data for each in eval_h])
+
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                out, eval_h = self.model(inputs, eval_h)
+                eval_loss = self.criterion(out.squeeze(), labels.float())
+
+                ev_ls.append(eval_loss)
+                accuracy = TrainPipeline.accuracy(out, labels)
+                ev_acc += accuracy / len(test_loader.dataset)
+
+            ep_tr_ls = np.mean(tr_ls)
+            ep_ev_ls = np.mean(ev_ls)
+            self.train_loss.append(ep_tr_ls)
+            self.eval_loss.append(ep_ev_ls)
+            self.train_acc.append(tr_acc)
+            self.eval_acc.append(ev_acc)
+
+            print(f"Train loss: {ep_tr_ls}  Evaluation loss: {ep_ev_ls}")
+            print(f"Train accuracy: {tr_acc}    Evaluation accuracy: {ev_acc}")
+
+            if ep_ev_ls <= self.best_loss:
+                torch.save(self.model.state_dict(), os.path.join(model_path, 'best_model.pt'))
+
+                self.best_loss = ep_ev_ls
+
+        print('='*25)
